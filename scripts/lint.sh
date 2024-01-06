9d098b1586ef9c8b04e59b70d0a2c075071f7a75
Nikita Pastukhov <diementros@yandex.ru>
2024-01-06 13:42:17 +0300

    chore: update dependencies

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-echo "Running pyup_dirs..."
-pyup_dirs --py38-plus --recursive taskiq_faststream tests
-
 echo "Running ruff..."
 ruff taskiq_faststream tests --fix
 
-echo "Running black..."
-black taskiq_faststream tests
+echo "Running ruff formatter..."
+ruff format taskiq_faststream tests
 
 echo "Running mypy..."
 mypy taskiq_faststream
---
+++ tests/test_redis.py
@@ -0,0 +1,25 @@
+import pytest
+from faststream import FastStream
+from faststream.redis import RedisBroker, TestRedisBroker
+from taskiq import AsyncBroker
+
+from taskiq_faststream import AppWrapper
+
+from .testcase import SchedulerTestcase
+
+
+@pytest.fixture
+def broker() -> RedisBroker:
+    return RedisBroker()
+
+
+class TestBroker(SchedulerTestcase):
+    test_class = TestRedisBroker
+    subj_name = "channel"
+
+
+class TestApp(TestBroker):
+    @staticmethod
+    def build_taskiq_broker(broker: RedisBroker) -> AsyncBroker:
+        """Build AppWrapper."""
+        return AppWrapper(FastStream(broker))
